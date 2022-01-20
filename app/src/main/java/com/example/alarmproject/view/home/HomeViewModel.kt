944957e23e117ca95072e201684c250da1fd6c1f
4538cgy <58514224+4538cgy@users.noreply.github.com>
2022-01-20 18:18:36 +0900

    clean code

--- app/src/main/java/com/example/alarmproject/view/home/HomeViewModel.kt
+++ app/src/main/java/com/example/alarmproject/view/home/HomeViewModel.kt
@@ -20,16 +20,13 @@ class HomeViewModel @Inject constructor() : BaseViewModel() {
     val alarmDataFlow2 = _alarmDataFlow2.asEventFlow()
 
     suspend fun getAlarmData() {
-        println("테스트 getAlarmData")
         _alarmDataFlow.emit(Result.Success(alarmTestData()))
     }
     suspend fun getAlarmData2(){
-        println("테스트 getAlarmData2")
         _alarmDataFlow2.emit(Result.Success(alarmTestData2()))
     }
 
     private fun alarmTestData() : AlarmList{
-        println("테스트 alarmTestData")
         val list = AlarmList()
         val alarmList = list.alarmList
         alarmList.add(AlarmTag("tttt",25200000))
@@ -39,7 +36,6 @@ class HomeViewModel @Inject constructor() : BaseViewModel() {
     }
 
     private fun alarmTestData2() : AlarmList{
-        println("테스트 alarmTestData2")
         val list = AlarmList()
         val alarmList = list.alarmList
         alarmList.add(AlarmTag("tttt2222",70200000))
