7ba5ef3c8432f8d6bd4db18a4234c60f96340cf1
jisungbin <ji@sungb.in>
2022-02-05 17:07:56 +0900

    stackoverflow!

--- app/src/main/java/com/example/alarmproject/util/receiver/AlarmReceiver.kt
+++ app/src/main/java/com/example/alarmproject/util/receiver/AlarmReceiver.kt
@@ -8,6 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
+import android.os.PowerManager
 import androidx.core.app.NotificationCompat
 import com.example.alarmproject.R
 import com.example.alarmproject.view.main.MainActivity
@@ -53,6 +54,9 @@ class AlarmReceiver : BroadcastReceiver() {
     // https://stackoverflow.com/questions/30199380/how-to-show-android-notifications-on-screen-as-well-as-status-bar-icon
     // https://stackoverflow.com/questions/35994055/show-notifications-on-screen-in-android
     // https://stackoverflow.com/questions/7442670/android-how-to-show-notification-on-screen
+    // https://stackoverflow.com/questions/2891337/turning-on-screen-programmatically
+    // https://stackoverflow.com/questions/9631869/light-up-screen-when-notification-received-android
+    // https://stackoverflow.com/questions/54608626/how-to-wake-up-locked-screen-after-notification-popup-in-api-level-28
     private fun deliverNotification(context: Context) {
         val contentIntent = Intent(context, MainActivity::class.java)
         val contentPendingIntent = PendingIntent.getActivity(
@@ -78,6 +82,17 @@ class AlarmReceiver : BroadcastReceiver() {
             .setDefaults(NotificationCompat.DEFAULT_ALL)
 
         notificationManager.notify(NOTIFICATION_ID, builder.build())
+
+        val pm = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+        val isScreenOn = pm.isInteractive
+
+        if (!isScreenOn) {
+            val wl = pm.newWakeLock(
+                PowerManager.PARTIAL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP,
+                context.getString(R.string.app_name) // unique require
+            )
+            wl.acquire((ALARM_TIMER * 1000).toLong())
+        }
     }
 
     companion object {
