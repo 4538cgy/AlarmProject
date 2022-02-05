ee90844f0f15d44bdae6e27bac1a1b7a351ad795
jisungbin <ji@sungb.in>
2022-02-05 17:18:25 +0900

    Add open wakeup activity

--- app/src/main/java/com/example/alarmproject/util/receiver/AlarmReceiver.kt
+++ app/src/main/java/com/example/alarmproject/util/receiver/AlarmReceiver.kt
@@ -1,5 +1,6 @@
 package com.example.alarmproject.util.receiver
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -8,56 +9,48 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.media.RingtoneManager
-import android.os.Build
 import android.os.PowerManager
 import androidx.core.app.NotificationCompat
 import com.example.alarmproject.R
 import com.example.alarmproject.view.main.MainActivity
+import com.example.alarmproject.view.wakeup.WakeupActivity
 
 class AlarmReceiver : BroadcastReceiver() {
 
     private lateinit var notificationManager: NotificationManager
 
-    override fun onReceive(p0: Context?, p1: Intent?) {
-        notificationManager = p0?.getSystemService(
-            Context.NOTIFICATION_SERVICE
-        ) as NotificationManager
+    override fun onReceive(context: Context?, p1: Intent?) {
+        notificationManager =
+            context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         println("브로드 캐스트 리시버")
 
         createNotificationChannel()
-        deliverNotification(p0)
+        deliverNotification(context)
+        context.startActivity(Intent(context, WakeupActivity::class.java))
     }
 
     private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                CHANNEL_ID, // 채널의 아이디
-                "채널 이름입니다.", // 채널의 이름
-                NotificationManager.IMPORTANCE_HIGH
-                /*
-                1. IMPORTANCE_HIGH = 알림음이 울리고 헤드업 알림으로 표시
-                2. IMPORTANCE_DEFAULT = 알림음 울림
-                3. IMPORTANCE_LOW = 알림음 없음
-                4. IMPORTANCE_MIN = 알림음 없고 상태줄 표시 X
-                 */
-            )
-            notificationChannel.enableLights(true) // 불빛
-            notificationChannel.lightColor = Color.RED // 색상
-            notificationChannel.enableVibration(true) // 진동 여부
-            notificationChannel.description = "채널의 상세정보입니다." // 채널 정보
-            notificationManager.createNotificationChannel(
-                notificationChannel
-            )
-        }
+        val notificationChannel = NotificationChannel(
+            CHANNEL_ID, // 채널의 아이디
+            "채널 이름입니다.", // 채널의 이름
+            NotificationManager.IMPORTANCE_HIGH
+            /*
+            1. IMPORTANCE_HIGH = 알림음이 울리고 헤드업 알림으로 표시
+            2. IMPORTANCE_DEFAULT = 알림음 울림
+            3. IMPORTANCE_LOW = 알림음 없음
+            4. IMPORTANCE_MIN = 알림음 없고 상태줄 표시 X
+             */
+        )
+        notificationChannel.enableLights(true) // 불빛
+        notificationChannel.lightColor = Color.RED // 색상
+        notificationChannel.enableVibration(true) // 진동 여부
+        notificationChannel.description = "채널의 상세정보입니다." // 채널 정보
+        notificationManager.createNotificationChannel(
+            notificationChannel
+        )
     }
 
-    // https://stackoverflow.com/questions/30090589/turn-on-screen-programmatically-on-android
-    // https://stackoverflow.com/questions/30199380/how-to-show-android-notifications-on-screen-as-well-as-status-bar-icon
-    // https://stackoverflow.com/questions/35994055/show-notifications-on-screen-in-android
-    // https://stackoverflow.com/questions/7442670/android-how-to-show-notification-on-screen
-    // https://stackoverflow.com/questions/2891337/turning-on-screen-programmatically
-    // https://stackoverflow.com/questions/9631869/light-up-screen-when-notification-received-android
-    // https://stackoverflow.com/questions/54608626/how-to-wake-up-locked-screen-after-notification-popup-in-api-level-28
+    @SuppressLint("UnspecifiedImmutableFlag")
     private fun deliverNotification(context: Context) {
         val contentIntent = Intent(context, MainActivity::class.java)
         val contentPendingIntent = PendingIntent.getActivity(
